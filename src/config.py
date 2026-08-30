8614c003dc119f8bb536e43988ff8232e929a088
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-08-30 17:05:36 +0200

    adding the config file

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+project_root=Path(__file__).resolve().parent.parent
+
+#sql db location
+db_loc=project_root/"data"/"database"/"orders.db"
+
+# Vectore database location
+chroma_loc=str(project_root/"storage"/"vector_databases")
+
+
+# knowledge documents location
+kd_loc=project_root/"data"/"policy_docs"/"ecommerce_customer_support_knowledge_base.md"
+
