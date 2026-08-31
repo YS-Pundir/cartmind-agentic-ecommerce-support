865ca9006e711971686d88ff2c3b0b01b4e0901d
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-08-31 13:18:51 +0200

    modified the retrieval logic for generation usage

--- src/rag/retrieval.py
+++ src/rag/retrieval.py
@@ -4,8 +4,10 @@ from src.rag.embeddings import embedding_model
 from src.config import chroma_loc
 query="How many loyalty points are needed for the minimum redemption, and what is the cash conversion rate?"
 
+collection_name=input("Please enter the collection name you want to use :-- ",)
+
 vectorestore_persisted=Chroma(
-    collection_name="kb_fixed_size",
+    collection_name=collection_name,
     persist_directory=chroma_loc,
     embedding_function=embedding_model
 )
@@ -17,8 +19,4 @@ retreiver=vectorestore_persisted.as_retriever(
 
 def retrieve_chunks(user_query, retriever):
     docs = retriever.invoke(user_query)
-    return [
-        {"index": i, "text": d.page_content, "metadata": d.metadata}
-        for i, d in enumerate(docs, start=1)
-    ]
-pprint(retrieve_chunks(query,retreiver))
+    return docs
