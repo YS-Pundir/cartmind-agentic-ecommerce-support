5d7a2a2bca88e3addb2f43f2d6402ed8d1699344
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-09-01 20:40:01 +0200

    adding some minor changes in retrival related to the collection name changes after the outcome of task 4 and task 5

--- src/rag/retrieval.py
+++ src/rag/retrieval.py
@@ -2,9 +2,9 @@ from pprint import pprint
 from langchain_community.vectorstores import Chroma
 from src.rag.embeddings import embedding_model
 from src.config import chroma_loc
-query="How many loyalty points are needed for the minimum redemption, and what is the cash conversion rate?"
 
-collection_name=input("Please enter the collection name you want to use :-- ",)
+
+collection_name="kb_fixed_size"  # choosen from task 5!!!
 
 vectorestore_persisted=Chroma(
     collection_name=collection_name,
