a3862fdbe395392241880b412ae90e07c237abc4
YS-Pundir <yuvrajpundir2005@gmail.com>
2026-09-07 16:36:11 +0200

    modifing the storage logic

--- src/tools/deffered_tool.py
+++ src/tools/deffered_tool.py
@@ -9,31 +9,11 @@ deferred_cases = pd.read_csv(deferred_case_location)
 def defer_to_human( query: str, intent: str,record_id:str) -> str:
 
     global deferred_cases
-    while True:
-        try:
-            phone_input = input("Please enter your phone no (10 digits): ")
-        
-            # 1. Check if the input contains only digits and is exactly 10 characters long
-            if not phone_input.isdigit() or len(phone_input) != 10:
-                print("Invalid input! Phone number must be exactly 10 digits and contain numbers only. Try again.\n")
-                continue
-            
-            # 2. Convert to int datatype as requested
-            phone_no = int(phone_input)
-            masked_no=mask_pii(str(phone_no))
-        
-            print(f"Successfully accepted phone number: {masked_no}")
-            break
-        
-        except ValueError:
-            print("Invalid datatype! Please enter numbers only.\n")
-
     case_entry = {
         "timestamp": datetime.now(),
         "record_id": record_id,
         "query": query,
-        "intent": intent,
-        "contact number":phone_no
+        "intent": intent
     }
     deferred_cases = pd.concat([deferred_cases, pd.DataFrame([case_entry])], ignore_index=True)
     deferred_cases.to_csv(deferred_case_location, index=False)
